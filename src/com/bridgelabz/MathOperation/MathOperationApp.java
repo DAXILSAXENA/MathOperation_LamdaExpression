d377ab6c7d51cd5aefc1298503169b45f5ab6053
DAXILSAXENA <daxildaxil@gmail.com>
2021-07-05 11:42:08 +0530

    Added Welcome message

---
+++ src/com/bridgelabz/MathOperation/MathOperationApp.java
@@ -0,0 +1,8 @@
+package com.bridgelabz.MathOperation;
+
+public class MathOperationApp {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Math Operation App using Lambda Expression:");
+	}
+}
